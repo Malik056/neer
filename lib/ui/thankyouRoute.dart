b34c8a5b31505f31bd88fa0897e1f3d7fe92a88f
Muhammad Taha <tahamahmoodmalik@gmail.com>
2020-08-17 15:12:21 +0500

    Added Bottom Navigation to Thank you route

--- lib/ui/thankyouRoute.dart
+++ lib/ui/thankyouRoute.dart
@@ -13,27 +13,60 @@ class ThankYouRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      persistentFooterButtons: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 130,
-              height: 40,
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4.0)),
-                color: Colors.black,
-                textColor: Colors.white,
-                child: Text('Done'),
+      // persistentFooterButtons: <Widget>[
+      //   Row(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       Container(
+      //         width: 130,
+      //         height: 40,
+      //         child: RaisedButton(
+      //           onPressed: () {
+      //             Navigator.pop(context);
+      //           },
+      //           shape: RoundedRectangleBorder(
+      //               borderRadius: BorderRadius.circular(4.0)),
+      //           color: Colors.black,
+      //           textColor: Colors.white,
+      //           child: Text('Done'),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ],
+      //REMOVED: Done Button /\ Replaced Done button with bottom navigation
+      ///////////////////////||////////////////////////////////////////////
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          Navigator.pushAndRemoveUntil(
+              context,
+              CupertinoPageRoute(
+                builder: (ctx) => HomeScreenRoute(
+                  initialIndex: index,
+                ),
               ),
-            ),
-          ],
-        ),
-      ],
+              (route) => false);
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.work),
+            title: Text('Jobs'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Profile'),
+          ),
+        ],
+      ),
       body: Padding(
         padding: EdgeInsets.only(
           top: MediaQuery.of(context).padding.top + 20.0,
